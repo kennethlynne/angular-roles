8c3580c81b51c4fdd26fdef2ee2e6e6211b2ca97
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-20 19:00:47 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-angular-roles
-=============
+ng-roles
+=============================
 
-Angular roles provider
+Access control library for angular applications
+
+`bower install ng-roles --save`
+
+# Usage
+## Permissions
+```javascript
+accessControl.setPermissions(['roll', 'eat', 'sleep']);
+
+expect(accessControl.can('wear socks')).toBeFalsy();
+expect(accessControl.can('sleep')).toBeTruthy();
+```
+
+## Roles
+```javascript
+accessControl.setRoles(['superadmin', 'user']);
+
+expect(accessControl.is('god')).toBeFalsy();
+expect(accessControl.is('superadmin')).toBeTruthy();
+expect(accessControl.is('user')).toBeTruthy();
+```
+
+## Unset
+```javascript
+//Unset will clear everything
+accessControl.unset();
+expect(accessControl.getPermissions()).toEqual([]);
+expect(accessControl.getRoles()).toEqual([]);
+```
+
+# Use in views
+You can expose the accessControl service in views (in the controller, or on $rootScope).
+Then you are able to hide elements based on the current users permissions.
+
+```html
+<button ng-click="delete()" ng-show="User.is('administrator')">Delete something<button>
+```
---
+++ ng-roles.src.js
@@ -0,0 +1,48 @@
+angular.module('ngRoles', [])
+    .factory('accessControl', function () {
+
+        var storage = {};
+
+        var _permissions = storage.permissions = storage.permissions || [];
+        var _roles = storage.roles = storage.roles || [];
+
+        var _set = function (user) {
+                _setRoles(user.roles);
+                _setPermissions(user.permissions);
+
+                angular.copy(user, _user);
+            },
+            _unset = function () {
+                _setRoles([]);
+                _setPermissions([]);
+            },
+            _is = function (role) {
+                return _roles.indexOf(role) >= 0;
+            },
+            _setRoles = function (roles) {
+                angular.copy(roles, _roles);
+            },
+            _getRoles = function () {
+                return angular.copy(_roles);
+            },
+            _setPermissions = function (permissions) {
+                angular.copy(permissions, _permissions);
+            },
+            _getPermissions = function () {
+                return angular.copy(_permissions);
+            },
+            _can = function (thing) {
+                return _permissions.indexOf(thing) >= 0;
+            };
+
+        return {
+            set: _set,
+            unset: _unset,
+            can: _can,
+            is: _is,
+            setRoles: _setRoles,
+            getRoles: _getRoles,
+            setPermissions: _setPermissions,
+            getPermissions: _getPermissions
+        }
+    });
